3e46d55d2a11b041fc7d1fe894e53aa0517679d0
JE72819 <kevinmichael.hagel@kbc.be>
2021-07-24 08:54:03 +0300

    Initial commit

---
+++ src/main/java/com/bccdb/rest/BccdbRestApplication.java
@@ -0,0 +1,13 @@
+package com.bccdb.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BccdbRestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BccdbRestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bccdb/rest/BccdbRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bccdb.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BccdbRestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
